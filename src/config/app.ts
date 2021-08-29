986202a80fd64d9f74f1faf1cb72d2f3a548f3bb
Gabriel Grazionale <gabriel.grazionale@portalsolar.com.br>
2021-08-29 10:33:50 -0300

    chore: config express middlewares and cors

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,9 +1,11 @@
 
 import express from 'express'
 import setupRoutes from './setup-routes'
+import setupMiddlewares from './setup-middlewares'
 
 const app = express()
 
+setupMiddlewares(app)
 setupRoutes(app)
 
 export default app
