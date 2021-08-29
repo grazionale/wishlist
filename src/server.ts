6497a565f5ccd31197f6aeaac690303ae8963af6
Gabriel Grazionale <gabriel.grazionale@portalsolar.com.br>
2021-08-29 11:09:25 -0300

    chore: change server console url

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
 import env from './config/env'
 import app from './config/app'
 
-app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
+app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}/api`))
