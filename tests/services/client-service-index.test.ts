6d14330ca5b202c583f9ec94ea069f751488f00d
Gabriel Grazionale <gabriel.grazionale@portalsolar.com.br>
2021-08-30 08:16:59 -0300

    test: client service

---
+++ tests/services/client-service-index.test.ts
@@ -0,0 +1,18 @@
+import FakeClientRepository from '../../src/app/repositories/fakes/fake-client-repository'
+import ClientService from '../../src/app/services/client-service'
+
+describe('ClientService', () => {
+  let fakeClientRepository: FakeClientRepository
+  let clientService: ClientService
+
+  beforeEach(() => {
+    fakeClientRepository = new FakeClientRepository()
+    clientService = new ClientService(fakeClientRepository)
+  })
+
+  it('should be list of clients', async () => {
+    const listOfClients = await clientService.index()
+
+    expect(listOfClients).toHaveLength(1)
+  })
+})
