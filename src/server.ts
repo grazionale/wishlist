5ecdb53092287ed50bb663494ad597f5f3775dad
Gabriel Grazionale <gabriel.grazionale@portalsolar.com.br>
2021-08-30 19:36:15 -0300

    chore: update server to load app after database

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import 'reflect-metadata'
 import 'express-async-errors'
 import env from './config/env'
-import app from './config/app'
 import SetupDatabase from './config/setup-database'
 
 const setupDatabase = new SetupDatabase()
 
 setupDatabase.handle().then(async () => {
+  const app = (await import('./config/app')).default
   app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}/api`))
 }).catch(error => console.log(error))
