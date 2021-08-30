c3887957c6f97b4232064df5a158e98c9e4c830c
Gabriel Grazionale <gabriel.grazionale@portalsolar.com.br>
2021-08-30 08:21:17 -0300

    feat: add client routes

---
+++ src/routes/client-routes.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express'
+import ClientsController from '../app/controllers/ClientController'
+
+const clientsController = new ClientsController()
+
+export default (router: Router): void => {
+  router.get('/clients', clientsController.index)
+}
