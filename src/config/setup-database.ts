3b360d6a1ab3acb8e6b3848d6bed670910ba0f5b
Gabriel Grazionale <gabriel.grazionale@portalsolar.com.br>
2021-08-31 09:47:39 -0300

    chore: permit setup-database receive customOrmConfig

--- src/config/setup-database.ts
+++ src/config/setup-database.ts
@@ -1,9 +1,22 @@
-import { Connection, createConnection } from 'typeorm'
-
+import { Connection, ConnectionOptions, createConnection } from 'typeorm'
 export default class SetupDatabase {
+  private readonly customOrmConfig?: ConnectionOptions
+
+  constructor (
+    customOrmConfig?: ConnectionOptions
+  ) {
+    this.customOrmConfig = customOrmConfig
+  }
+
   public async handle (): Promise<Connection | null> {
+    let conn: Connection
     try {
-      const conn = await createConnection()
+      if (this.customOrmConfig) {
+        conn = await createConnection(this.customOrmConfig)
+      } else {
+        // Por padrão, TypeORM busca o arquivo ormconfig.ts na raiz do projeto
+        conn = await createConnection()
+      }
       console.log(`Database connection success. Connection name: '${conn.name}' Database: '${conn.options.database}'`)
     } catch (err) {
       console.log(err)
