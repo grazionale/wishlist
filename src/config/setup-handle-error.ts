f93bded45efc194e1b717df823067ceefbc6bfb3
Gabriel Grazionale <gabriel.grazionale@portalsolar.com.br>
2021-09-01 15:21:08 -0300

    fix: returning correct status code in setup handle error

--- src/config/setup-handle-error.ts
+++ src/config/setup-handle-error.ts
@@ -4,12 +4,12 @@ import AppError from '../app/errors/app-error'
 export default (app: Express): void => {
   app.use((err: Error, _request: Request, response: Response, _: NextFunction) => {
     if (err instanceof AppError) {
-      return response.status(err.statusCode).json({ status: 'error', message: err.message })
+      return response.status(err.statusCode).json({ status: err.statusCode, message: err.message })
     }
     console.log(err)
 
     return response.status(500).json({
-      status: 'error',
+      status: 500,
       message: 'Internal server error'
     })
   })
