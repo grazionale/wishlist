ecf240c88b9e626b45a30f9d7cf8d0a7850656b6
Gabriel Grazionale <gabriel.grazionale@portalsolar.com.br>
2021-08-31 22:47:23 -0300

    fix: import files in client repository

--- src/app/interfaces/repositories/client-repository.ts
+++ src/app/interfaces/repositories/client-repository.ts
@@ -1,11 +1,11 @@
-import IClientShowDTO from '../../dtos/services/client-service-show-dto'
-import IClientIndexDTO from '../../dtos/repositories/client-repository-index-dto'
+import IClientShowResponseDTO from '../../dtos/services/client-service-show-response-dto'
+import IClientIndexResponseDTO from '../../dtos/repositories/client-repository-index-response-dto'
 import IClientPostRequestDTO from '../../dtos/repositories/client-repository-post-request-dto'
 import IClientPostResponseDTO from '../../dtos/repositories/client-repository-post-response-dto'
 
 export interface IClientRepository {
-  index: () => Promise<IClientIndexDTO[]>
-  show: (clientId: string) => Promise<IClientShowDTO | undefined>
-  findByEmail: (clientEmail: string) => Promise<IClientShowDTO | undefined>
+  index: () => Promise<IClientIndexResponseDTO[]>
+  show: (clientId: string) => Promise<IClientShowResponseDTO | undefined>
+  findByEmail: (clientEmail: string) => Promise<IClientShowResponseDTO | undefined>
   create: (client: IClientPostRequestDTO) => Promise<IClientPostResponseDTO>
 }
