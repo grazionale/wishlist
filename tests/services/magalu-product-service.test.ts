16492a3e28484093cf9d2bf2210584e4f98271db
Gabriel Grazionale <gabriel.grazionale@portalsolar.com.br>
2021-09-01 20:22:38 -0300

    test: 404 and 500 status in magalu product service

--- tests/services/magalu-product-service.test.ts
+++ tests/services/magalu-product-service.test.ts
@@ -1,3 +1,5 @@
+import nock from 'nock'
+import AppError from '../../src/app/errors/app-error'
 import MagaluProductService from '../../src/app/services/magalu-product-service'
 import apiMagalu from '../../src/config/setup-api-magalu'
 
@@ -6,7 +8,7 @@ const mockMagaluShowResponse =
     price: 1699,
     image: 'http://challenge-api.luizalabs.com/images/1bf0f365-fbdd-4e21-9786-da459d78dd1f.jpg',
     brand: 'bébé confort',
-    id: '1bf0f365-fbdd-4e21-9786-da459d78dd1f',
+    id: '123-123-123-123',
     title: 'Cadeira para Auto Iseos Bébé Confort Earth Brown'
   }
 
@@ -25,5 +27,23 @@ describe('FavoriteService', () => {
     const product = await magaluProductService.show('123-123-123-123')
 
     expect(product).toEqual(mockMagaluShowResponse)
+  })
+
+  it('should be return 404 with inexistent product id', async () => {
+    nock('http://challenge-api.luizalabs.com')
+      .get('/api/product/id_inexistente/')
+      .reply(404)
+
+    await expect(magaluProductService.show('id_inexistente'))
+      .rejects.toEqual(new AppError('magalu product not found', 404))
+  })
+
+  it('should be return 500', async () => {
+    nock('http://challenge-api.luizalabs.com')
+      .get('/api/product/id_inexistente/')
+      .reply(500)
+
+    await expect(magaluProductService.show('id_inexistente'))
+      .rejects.toEqual(new AppError('internal server error', 500))
   })
 })
