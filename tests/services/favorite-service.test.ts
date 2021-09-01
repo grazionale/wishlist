5bbe7159a0bae2d822c3403ffcd84044a97efdb1
Gabriel Grazionale <gabriel.grazionale@portalsolar.com.br>
2021-09-01 20:21:48 -0300

    refactor: remove comment from code

--- tests/services/favorite-service.test.ts
+++ tests/services/favorite-service.test.ts
@@ -28,7 +28,7 @@ const mockMagaluShowResponse =
     price: 1699,
     image: 'http://challenge-api.luizalabs.com/images/1bf0f365-fbdd-4e21-9786-da459d78dd1f.jpg',
     brand: 'bébé confort',
-    id: '1bf0f365-fbdd-4e21-9786-da459d78dd1f',
+    id: '123-123-123-123-123',
     title: 'Cadeira para Auto Iseos Bébé Confort Earth Brown'
   }
 
@@ -37,7 +37,7 @@ describe('FavoriteService', () => {
   let favoriteService: FavoriteService
   let clientService: ClientService
   let fakeClientRepository: FakeClientRepository
-  let magaluProductService: MagaluProductService // TO DO - Mockar
+  let magaluProductService: MagaluProductService
 
   beforeEach(() => {
     fakeFavoriteRepository = new FakeFavoriteRepository()
