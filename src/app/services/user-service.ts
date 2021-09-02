1b116f4e3752d0e6c0b32e8524cf63a677611cd4
Gabriel Grazionale <gabriel.grazionale@portalsolar.com.br>
2021-09-02 11:51:27 -0300

    feat: should be not insert one user with e-mail already created

--- src/app/services/user-service.ts
+++ src/app/services/user-service.ts
@@ -1,3 +1,4 @@
+import AppError from '../errors/app-error'
 import IUserCreateResponseDTO from '../dtos/services/user/user-service-create-response-dto'
 import { IUserRepository } from '../interfaces/repositories/user/user-repository'
 import IUserCreateRequestDTO from '../dtos/services/user/user-service-create-request-dto'
@@ -13,6 +14,12 @@ class UserService {
   }
 
   public async create (user: IUserCreateRequestDTO): Promise<IUserCreateResponseDTO> {
+    const findUser = await this.userRepository.findByUsername(user.username)
+
+    if (findUser) {
+      throw new AppError('username already used.')
+    }
+
     const userSaved = await this.userRepository.create(user)
 
     return userSaved
