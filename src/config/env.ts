756c5ef2739bec17fd7345f23e7d583a7fe498f5
Gabriel Grazionale <gabriel.grazionale@portalsolar.com.br>
2021-09-02 14:49:36 -0300

    chore: define json web token enviroment variables

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,7 @@
 import './bootstrap'
 
 export default {
-  port: process.env.PORT || 3333
+  port: process.env.PORT || 3333,
+  jwt_secret: process.env.JWT_SECRET || 'default_test',
+  jwt_expiresIn: process.env.JWT_EXPIRES_IN || '1d'
 }
