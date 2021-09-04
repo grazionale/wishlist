bf575a89db53f83d01998ecf5fc87366a03b3e37
Gabriel Grazionale <gabriel.grazionale@portalsolar.com.br>
2021-09-04 14:26:00 -0300

    feat: update product in product external queue

--- src/app/queues/product-external-queue.ts
+++ src/app/queues/product-external-queue.ts
@@ -1,8 +1,19 @@
 import RabbitmqServer from '../../config/setup-rabbitmq'
+import { makeProductService } from '../factories/services/product-service-factory'
 
 export default (server: RabbitmqServer): void => {
+  const productServide = makeProductService()
+
+  const updateProduct = async (message): Promise<void> => {
+    try {
+      await productServide.update(JSON.parse(message.content.toString()))
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   server.start().then(async () => {
-    await server.consume('product-external', (message) => console.log(message.content.toString()))
+    await server.consume('product-external', updateProduct)
   }).catch(err => {
     console.log(err)
   })
