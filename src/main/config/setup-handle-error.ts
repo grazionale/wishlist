6c584ad8f8b8852f273d21e497ffef73468c6309
Gabriel Grazionale <gabriel.grazionale@portalsolar.com.br>
2021-09-11 12:42:29 -0300

    feat: add logger

--- src/main/config/setup-handle-error.ts
+++ src/main/config/setup-handle-error.ts
@@ -1,12 +1,15 @@
 import { Express, NextFunction, Request, Response } from 'express'
 import AppError from '../../presentation/errors/app-error'
+import logger from './setup-logger'
 
 export default (app: Express): void => {
   app.use((err: Error, _request: Request, response: Response, _: NextFunction) => {
     if (err instanceof AppError) {
+      logger.info(JSON.stringify(err))
       return response.status(err.statusCode).json({ status: err.statusCode, message: err.message })
     }
-    console.log(err)
+
+    logger.error(JSON.stringify(err))
 
     return response.status(500).json({
       status: 500,
